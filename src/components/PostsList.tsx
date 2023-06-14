93ecf693190d2147dc1f2c9851e8f38e1424e8e0
robmainah <44176348+robmainah@users.noreply.github.com>
2023-06-14 08:02:19 +0300

    fetch data from server

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState, ChangeEvent } from "react";
+import { Fragment, useState, ChangeEvent, useEffect } from "react";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
@@ -13,8 +13,26 @@ interface PostsListInterface {
 const PostsList = ({ isPosting, onStopPosting }: PostsListInterface) => {
   const [posts, setPosts] = useState<PostInterface[]>([]);
 
-  const addNewPost = (data: PostInterface) => {
+  const addNewPost = async (data: PostInterface) => {
+    await fetch("http://localhost:8080/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
     setPosts((existingPosts) => [data, ...existingPosts]);
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = async () => {
+    const response = await fetch("http://localhost:8080/posts");
+    const { posts } = await response.json();
+    setPosts(posts);
   };
 
   return (
@@ -25,11 +43,16 @@ const PostsList = ({ isPosting, onStopPosting }: PostsListInterface) => {
         </Modal>
       )}
 
-      <ul className={classes.posts}>
-        {posts.map((post) => (
-          <Post author={post.author} text={post.body} key={post.body} />
-        ))}
-      </ul>
+      {posts.length === 0 && (
+        <h2 style={{ textAlign: "center", color: "white" }}>No posts</h2>
+      )}
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post, index) => (
+            <Post author={post.author} text={post.body} key={index} />
+          ))}
+        </ul>
+      )}
     </Fragment>
   );
 };
